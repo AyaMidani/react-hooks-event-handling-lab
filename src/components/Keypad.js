87b4e5b34e43c1fdb5ca64e6bf7d6e2791720480
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-07-13 22:22:47 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+function handleChange(){
+    console.log("Entering password...")
+}
+  return (
+        <input
+        type="password"
+        name="search"
+        onChange={handleChange}
+        placeholder="Enter search term..."
+      />
+  );
+}
+
+export default Keypad;
